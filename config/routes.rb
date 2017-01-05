3b9f55f5c12a2f85c01684fd50ba342a2392e450
Profession-Programing <jjfisher91@gmail.com>
2017-01-04 20:04:59 -0600

    home index made

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # This is a test comment to test out using github
+  get "/home" => "homes#index"
 end
